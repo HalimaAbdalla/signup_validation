191380cb42e8e2d6dd20aa051dae0e53d7d34d1a
HalimaAbdalla <halima.cfk@gmail.com>
2018-08-15 15:37:57 +0300

    validation using python dictionaries

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, url_for
+from flask import request
+
+user_credentials = {}
+
 
 app = Flask (__name__)
 
@@ -6,9 +10,32 @@ app = Flask (__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/login_process', methods = ['GET', 'POST'])
+def login_process():
+    username = request.values['Username']
+    password = request.values['Password']
+    if username in user_credentials.keys():
+        if password == user_credentials[username][2]:
+            return render_template('home.html')
+        else:
+            return render_template('login-failed.html')
+    else:
+        return render_template('not-found.html')
+    
+
+@app.route('/signup.html')
 def signup():
     return render_template('signup.html')
+
+@app.route('/signup_process', methods = ['GET', 'POST'])
+def signup_process():
+    email = request.values['Email']
+    username = request.values['Username']
+    password = request.values['Password']
+
+    user_credentials[username] = (email,password)
+
+    return render_template('index.html')
 
 @app.route('/home')
 def home():
